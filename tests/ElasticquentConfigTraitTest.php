b0323749807186e55322ebca06b80368e13f24d2
Peter Coles <peter@kuroiweb.com>
2016-01-25 20:05:37 +0000

    Use trait directly in test class, allows visibility of getElasticConfig to be reverted to protected.

--- tests/ElasticquentConfigTraitTest.php
+++ tests/ElasticquentConfigTraitTest.php
@@ -2,15 +2,12 @@
 
 class ElasticquentConfigTraitTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        $this->model = new TestModel;
-    }
+    use Elasticquent\ElasticquentTrait;
 
     public function testAccesssToConfig()
     {
-        $this->assertEquals(['localhost:9200'], $this->model->getElasticConfig('config.hosts'));
-        $this->assertEquals(1, $this->model->getElasticConfig('config.retries'));
-        $this->assertEquals('my_custom_index_name', $this->model->getElasticConfig('default_index'));
+        $this->assertEquals(['localhost:9200'], $this->getElasticConfig('config.hosts'));
+        $this->assertEquals(1, $this->getElasticConfig('config.retries'));
+        $this->assertEquals('my_custom_index_name', $this->getElasticConfig('default_index'));
     }
 }
