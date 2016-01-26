0e5969bb8488370a4936a4336065065ff0aacd85
Peter Coles <peter@kuroiweb.com>
2016-01-26 17:48:48 +0000

    Missing property

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -50,6 +50,15 @@ trait ElasticquentTrait
     protected $documentVersion = null;
 
     /**
+     * Mapping Properties
+     *
+     * Elasticsearch mapping properties
+     *
+     * @var null|array
+     */
+    protected $mappingProperties = null;
+
+    /**
      * New Collection
      *
      * @param array $models
