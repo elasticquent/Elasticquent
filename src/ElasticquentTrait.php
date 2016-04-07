b53834ffc037a4ff10a7a0cb2dd2cfdda9add3ef
Li Chuangbo <im@chuangbo.li>
2016-04-07 19:57:17 +1200

    Load eager relations and pivot recursive

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -3,6 +3,8 @@
 namespace Elasticquent;
 
 use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 /**
  * Elasticquent Trait
@@ -583,11 +585,10 @@ trait ElasticquentTrait
      */
     public function newFromHitBuilder($hit = array())
     {
-        $instance = $this->newInstance(array(), true);
-        $keyname = $instance->getKeyName();    
+        $keyname = $this->getKeyName();
         
-        $attributes = $hit['_source'];                        
-        $attributes[$keyname] = $hit['_id'];
+        $attributes = $hit['_source'];
+        $attributes[$keyname] = is_numeric($hit['_id']) ? intval($hit['_id']) : $hit['_id'];
         
         // Add fields to attributes
         if (isset($hit['fields'])) {
@@ -596,7 +597,7 @@ trait ElasticquentTrait
             }
         }
 
-        $instance->setRawAttributes((array)$attributes, true);
+        $instance = $this::newFromBuilderRecursive($this, $attributes);
 
         // In addition to setting the attributes
         // from the index, we will set the score as well.
@@ -614,4 +615,107 @@ trait ElasticquentTrait
         return $instance;
     }
 
+    /**
+     * Create a new model instance that is existing recursive.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  array  $attributes
+     * @param  \Illuminate\Database\Eloquent\Relation  $parentRelation
+     * @return static
+     */
+    public static function newFromBuilderRecursive(Model $model, array $attributes = [], Relation $parentRelation = null)
+    {
+        $instance = $model->newInstance([], $exists = true);
+
+        $instance->setRawAttributes((array)$attributes, $sync = true);
+
+        // Load relations recursive
+        static::loadRelationsAttributesRecursive($instance);
+        // Load pivot
+        static::loadPivotAttribute($instance, $parentRelation);
+
+        return $instance;
+    }
+
+    /**
+     * Create a collection of models from plain arrays recursive.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  \Illuminate\Database\Eloquent\Relation  $parentRelation
+     * @param  array  $items
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function hydrateRecursive(Model $model, array $items, Relation $parentRelation = null)
+    {
+        $instance = $model;
+
+        $items = array_map(function ($item) use ($instance, $parentRelation) {
+            return static::newFromBuilderRecursive($instance, $item, $parentRelation);
+        }, $items);
+
+        return $instance->newCollection($items);
+    }
+
+    /**
+     * Get the relations attributes from a model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     */
+    public static function loadRelationsAttributesRecursive(Model $model)
+    {
+        $attributes = $model->getAttributes();
+
+        foreach ($attributes as $key => $value) {
+            if (method_exists($model, $key)) {
+                $relation = $model->$key();
+                if ($relation instanceof Relation) {
+                    // Check if the relation field is single model or collections
+                    if (!static::isMultiLevelArray($value)) {
+                        $value = [$value];
+                    }
+                    $models = static::hydrateRecursive($relation->getModel(), $value, $relation);
+                    // Unset attribute before match relation
+                    unset($model[$key]);
+                    $relation->match([$model], $models, $key);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the pivot attribute from a model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  \Illuminate\Database\Eloquent\Relation  $parentRelation
+     */
+    public static function loadPivotAttribute(Model $model, Relation $parentRelation = null)
+    {
+        $attributes = $model->getAttributes();
+
+        foreach ($attributes as $key => $value) {
+            if ($key === 'pivot') {
+                unset($model[$key]);
+                $pivot = $parentRelation->newExistingPivot($value);
+                $model->setRelation($key, $pivot);
+            }
+        }
+    }
+
+    /**
+     * Check if an array is multi-level array like [[id], [id], [id]].
+     *
+     * For detect if a relation field is single model or collections.
+     *
+     * @param  array  $array
+     * @return boolean
+     */
+    private static function isMultiLevelArray(array $array)
+    {
+        foreach ($array as $key => $value) {
+            if (!is_array($value)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
